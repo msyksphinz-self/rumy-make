afaea799e4ce8639573aca0c143a85794ff0fbbf
msyksphinz-self <msyksphinz.dev@gmail.com>
2019-11-04 23:52:24 +0800

    Func: add rumy-env.rb

---
+++ src/rumy-env.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/ruby
+
+def clear_env
+  ENV.clear
+end
+
+def add_env_path(env_data)
+  if ENV["PATH"] == NIL then
+    ENV["PATH"] = env_data
+  else
+    ENV["PATH"] += ":" + env_data
+  end
+end
+
+def add_env_ldpath(env_data)
+  if ENV["LD_LIBRARY_PATH"] == NIL then
+    ENV["LD_LIBRARY_PATH"] = env_data
+  else
+    ENV["LD_LIBRARY_PATH"] += ":" + env_data
+  end
+end
+
+
+def add_env(env_name, env_data)
+  ENV[env_name] = env_data
+end
+
+def show_env
+  ENV.each{|k, e|
+    puts "key = " + k + ", data = " + e
+  }
+end
