3bcbec07f88ec821c360265e3bccbcdd90026de2
msyksphinz-self <msyksphinz.dev@gmail.com>
2019-11-11 22:25:50 +0800

    Func: update rumy-test

--- src/rumy-test.rb
+++ src/rumy-test.rb
@@ -1,21 +1,31 @@
 #!/usr/bin/ruby
+# coding: utf-8
 
 $test_list = Hash.new
 
-def add_test(name, command)
-  test = Test.new(name, command)
+def add_test(name, command, logfile)
+  test = Test.new(name, command, logfile)
   $test_list[name] = test
 end
 
 def run_tests
-  $test_list.each{|test|
-    `#{test}`
+  $test_list.each{|key, value|
+    puts value.command
+    result = `#{value.command}`
+    fp = File.open(value.logfile, "w") {|f|
+      f.puts(result)
+    }
   }
 end
 
 class Test
-  def initialize(name, command)
+  def initialize(name, command, logfile)
     @name = name
     @command = command
+    @logfile = logfile
   end
+
+  attr_reader :name
+  attr_reader :command
+  attr_reader :logfile
 end
