3c345dae2acaa9a9d88fd0366bec8af097591990
msyksphinz-self <msyksphinz.dev@gmail.com>
2019-11-12 17:26:13 +0800

    Func: update rumy-test package

--- src/rumy-test.rb
+++ src/rumy-test.rb
@@ -9,13 +9,34 @@ def add_test(name, command, logfile)
 end
 
 def run_tests
+  max_str_len = $test_list.map{|key, test| test.name.size}.max
+
+  test_success = 0
+  test_failure = 0
+
   $test_list.each{|key, value|
-    puts value.command
-    result = `#{value.command}`
+    print "Executing " + value.name + " ..."
+    result_string = `#{value.command}`
+    res_value = $?
+    (max_str_len - value.name.size + 10).times do
+      print(' ')
+    end
+    if res_value.exitstatus == 1 then
+      puts "o"
+      test_success = test_success + 1
+    else
+      puts "x"
+      test_failure = test_failure + 1
+    end
     fp = File.open(value.logfile, "w") {|f|
-      f.puts(result)
+      f.puts(result_string)
     }
   }
+  puts "======================================"
+  puts "  RESULT"
+  puts "  success = " + test_success.to_s
+  puts "  failure = " + test_failure.to_s
+  puts "======================================"
 end
 
 class Test
