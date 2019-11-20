41a4c498f6b9e43e4e4a84f953fa5e8f91b302f2
msyksphinz-self <msyksphinz.dev@gmail.com>
2019-11-20 22:58:58 +0800

    Func: update build.rb

--- tests/emacs-26.3/build.rb
+++ tests/emacs-26.3/build.rb
@@ -2,11 +2,10 @@
 
 external_make "lib", "lib"
 external_make "lib-src", "lib-src"
-external_target "src", "src", ["lib-src"]
+external_target "src", "src"
 external_make "lisp", "lisp"
-external_make "info", "info"
 
 make_target :all do
   global
-  depends ["lib", "lib-src", "src", "lisp", "info"]
+  depends ["lib", "lib-src", "src", "lisp"]
 end
